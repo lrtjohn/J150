3b9115d92457fa6e309448dcac578b55cc3a2f3d
Runtian <lrtjohn@vt.edu>
2020-12-12 23:13:36 +0800

    change Current TX format, modify current compensate parameters

--- platform/pfal/isr/src/timer1_isr.c
+++ platform/pfal/isr/src/timer1_isr.c
@@ -12,11 +12,15 @@ int gtimer1test = 0;
 
 void PFAL_Timer1_ISR(void)
 {
+	double duty_ratio;
+	double temp_current;
 //	GpioDataRegs.GPBDAT.bit.GPIO59 = 1;
 #if(SYS_DEBUG == INCLUDE_FEATURE)
     gtimer1test++;
 #endif
 
+    duty_ratio = (double)(gSpwmPara.Duty) / 1250;
+    temp_current = ((double)(gCurrent_Struct.I_busCurrent_Ave) * BUSCURRENT_K + BUSCURRENT_B) * 10 * duty_ratio;
 	gSciAppProtocolTx_J150.workStatus 	= gSysStateFlag.j150SysStatus.all;
 	gSciAppProtocolTx_J150.sysStatus1 	= gSysStateFlag.sysRunningState;
 	gSciAppProtocolTx_J150.sysStatus2 	= (Uint16)((double)(gCurrent_Struct.I_busCurrent_Ave) * BUSCURRENT_K + BUSCURRENT_B);
@@ -25,12 +29,12 @@ void PFAL_Timer1_ISR(void)
 	gSciAppProtocolTx_J150.targetSpeed 	= gSciAppProtocolRx_J150.targetSpeed;
 	gSciAppProtocolTx_J150.currentSpeed = gEcapPara.gMotorSpeedEcap;
 	gSciAppProtocolTx_J150.busVoltage 	= (Uint16)((double)(gSysAnalogVar.single.var[updatePower270V_M].value) * BUSVOLTAGE_K + BUSVOLTAGE_B);
-	gSciAppProtocolTx_J150.busCurrent 	= (Uint16)(((double)(gCurrent_Struct.I_busCurrent_Ave) * BUSCURRENT_K + BUSCURRENT_B)*10);
+	gSciAppProtocolTx_J150.busCurrent 	= (Uint16)(temp_current);
 	gSciAppProtocolTx_J150.servoTemp 	= (Uint16)((double)(gSysAnalogVar.single.var[updateDriverTemp].value) * SERVO_TEMP_K + SERVO_TEMP_B);
 	gSciAppProtocolTx_J150.motorTemp 	= (Uint16)((double)(gSysAnalogVar.single.var[updateMotorTemp].value) * MOTOR_TEMP_K + MOTOR_TEMP_B);
 	gSciAppProtocolTx_J150.fwVersionNum = gSysVersionNum;
-	gSciAppProtocolTx_J150.fwVersionNum = gSpwmPara.restrictduty;
-//    gSciAppProtocolTx_J150.workMode 	= gSciAppProtocolRx_J150.workMode;
+//	gSciAppProtocolTx_J150.fwVersionNum = gSpwmPara.restrictduty;
+    gSciAppProtocolTx_J150.workMode 	= gSciAppProtocolRx_J150.workMode;
 	gSciAppProtocolTx_J150.RFU 			= gSpwmPara.Duty;
 
 	SCI_TX_PackData(gScibTxQue);
